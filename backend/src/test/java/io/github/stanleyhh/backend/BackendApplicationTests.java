d5b8e7aec5993a055b6f443bc7c80e05c19ca4b9
Stanislav Dering <stanislavdering@gmail.com>
2025-12-07 20:24:33 +0100

    Add main test

--- backend/src/test/java/io/github/stanleyhh/backend/BackendApplicationTests.java
+++ backend/src/test/java/io/github/stanleyhh/backend/BackendApplicationTests.java
@@ -1,7 +1,10 @@
 package io.github.stanleyhh.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class BackendApplicationTests {
@@ -10,4 +13,12 @@ class BackendApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void main_runs() {
+        try (var mocked = mockStatic(SpringApplication.class)) {
+            BackendApplication.main(new String[]{});
+            mocked.verify(() -> SpringApplication.run(BackendApplication.class, new String[]{})
+            );
+        }
+    }
 }
