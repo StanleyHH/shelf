ad2eadaef8707f63b5a2f2a26f766a2dbc3db4cb
Stanislav Dering <stanislavdering@gmail.com>
2025-12-07 22:15:53 +0100

    Add Show repository

---
+++ backend/src/main/java/io/github/stanleyhh/backend/repositories/ShowRepository.java
@@ -0,0 +1,9 @@
+package io.github.stanleyhh.backend.repositories;
+
+import io.github.stanleyhh.backend.domain.entities.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ShowRepository extends JpaRepository<Show, UUID> {
+}
