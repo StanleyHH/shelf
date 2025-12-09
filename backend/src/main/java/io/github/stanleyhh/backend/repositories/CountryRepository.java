f6920ef972332de35fbd6d9e5dfc19e0025194ec
Stanislav Dering <stanislavdering@gmail.com>
2025-12-09 20:42:44 +0100

    Remove unused methods

--- backend/src/main/java/io/github/stanleyhh/backend/repositories/CountryRepository.java
+++ backend/src/main/java/io/github/stanleyhh/backend/repositories/CountryRepository.java
@@ -3,12 +3,9 @@ package io.github.stanleyhh.backend.repositories;
 import io.github.stanleyhh.backend.domain.entities.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface CountryRepository extends JpaRepository<Country, UUID> {
 
     boolean existsBy();
-
-    Optional<Country> getCountryByName(String name);
 }
--- backend/src/main/java/io/github/stanleyhh/backend/repositories/GenreRepository.java
+++ backend/src/main/java/io/github/stanleyhh/backend/repositories/GenreRepository.java
@@ -3,12 +3,10 @@ package io.github.stanleyhh.backend.repositories;
 import io.github.stanleyhh.backend.domain.entities.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface GenreRepository extends JpaRepository<Genre, UUID> {
 
     boolean existsBy();
 
-    Optional<Genre> getGenreByName(String name);
 }
