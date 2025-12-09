834c1ab5d22800157e77e824933baa8e58667ec1
Stanislav Dering <stanislavdering@gmail.com>
2025-12-09 20:31:08 +0100

    Add fetched data handling to ShowPage

--- frontend/src/components/ShowCard.tsx
+++ frontend/src/components/ShowCard.tsx
@@ -1,22 +1,20 @@
+import type Show from '../entities/Show.ts';
 import ShowStatusLabel from './ShowStatusLabel.tsx';
 
 interface Props {
-  status: string;
+  show: Show;
 }
 
-export default function ShowCard({ status }: Readonly<Props>) {
+export default function ShowCard({ show }: Readonly<Props>) {
   return (
     <div className="flex flex-col gap-1">
-      <img
-        src="https://static.tvmaze.com/uploads/images/original_untouched/220/550800.jpg"
-        alt="Show Title"
-      />
+      <img src={show.imageUrl} alt={show.title} />
       <div className="flex items-start gap-2">
-        <p className="text-base font-bold text-sky-600">Show Title</p>
-        <ShowStatusLabel status={status} />
+        <p className="text-base/5 font-bold text-sky-600">{show.title}</p>
+        <ShowStatusLabel status={show.status} />
       </div>
       <p className="text-sm/4 text-[#999999]">
-        2024 &#8226; US &#8226; Crime, Drama, Thriller
+        {show.startYear} &#8226; US &#8226; Crime, Drama, Thriller
       </p>
     </div>
   );
--- frontend/src/pages/ShowsPage.tsx
+++ frontend/src/pages/ShowsPage.tsx
@@ -1,14 +1,32 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import ShowCard from '../components/ShowCard.tsx';
+import useShows from '../hooks/useShows.ts';
 
 export default function ShowsPage() {
+  const { data, fetchNextPage, hasNextPage } = useShows();
+
+  const fetchedShowsCount =
+    data?.pages.reduce((total, page) => total + page.content.length, 0) || 0;
+
   return (
     <>
       <p className="text-3xl">TV Shows</p>
-      <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4">
-        <ShowCard status="ONGOING" />
-        <ShowCard status="ON_BREAK" />
-        <ShowCard status="ENDED" />
-      </div>
+      <InfiniteScroll
+        next={fetchNextPage}
+        hasMore={hasNextPage}
+        loader={<p>Loading...</p>}
+        dataLength={fetchedShowsCount}
+        className="scrollbar-none"
+      >
+        <div className="mt-5 grid grid-cols-2 gap-5 md:grid-cols-4">
+          {data?.pages.map((page) => {
+            return page.content.map((show) => (
+              <ShowCard key={show.id} show={show} />
+            ));
+          })}
+        </div>
+      </InfiniteScroll>
     </>
   );
 }
