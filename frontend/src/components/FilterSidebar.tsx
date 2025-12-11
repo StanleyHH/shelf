012bfff65618743b23760ebc66a32235266e316e
Stanislav Dering <stanislavdering@gmail.com>
2025-12-11 19:33:40 +0100

    Add SearchFilter

--- frontend/src/components/FilterSidebar.tsx
+++ frontend/src/components/FilterSidebar.tsx
@@ -1,10 +1,14 @@
+import SearchFilter from './SearchFilter.tsx';
+
 export default function FilterSidebar() {
   return (
     <aside
       className="scrollbar-none fixed top-(--sidebar-y-indent)
         right-(--sidebar-x-indent) h-(--sidebar-height)
         w-(--filter-sidebar-width) overflow-y-auto border-l-2 border-gray-150
-        bg-white"
-    ></aside>
+        bg-white p-5"
+    >
+      <SearchFilter />
+    </aside>
   );
 }
