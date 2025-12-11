d4aa74cf2ea15cc00aab9ab90ea8d5c1a9a6dd04
Stanislav Dering <stanislavdering@gmail.com>
2025-12-11 19:33:22 +0100

    Update useShows to support query parameters

--- frontend/src/hooks/useShows.ts
+++ frontend/src/hooks/useShows.ts
@@ -3,15 +3,27 @@ import ms from 'ms';
 
 import type Show from '../entities/Show.ts';
 import ApiClient, { type FetchResponse } from '../services/apiClient.ts';
+import useShowQueryStore from '../store.ts';
 
 const apiClient = new ApiClient<Show>('/shows');
 
 const useShows = () => {
-  return useInfiniteQuery<FetchResponse<Show>>({
-    queryKey: ['shows'],
+  const showQuery = useShowQueryStore((s) => s.showQuery);
+
+  return useInfiniteQuery<FetchResponse<Show>, Error>({
+    queryKey: ['shows', showQuery],
     initialPageParam: 1,
     queryFn: ({ pageParam }) =>
-      apiClient.getAll({ params: { page: pageParam } }),
+      apiClient.getAll({
+        params: {
+          q: showQuery.searchText,
+          genre: showQuery.genreName,
+          country: showQuery.countryName,
+          status: showQuery.status,
+          year: showQuery.year,
+          page: pageParam,
+        },
+      }),
     getNextPageParam: (lastPage) => {
       const next = lastPage.page + 1;
       return next <= lastPage.totalPages ? next : undefined;
