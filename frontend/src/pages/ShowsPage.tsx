710d5c9e6b9caf00cbe2cf88d0e140a94a37a960
Stanislav Dering <stanislavdering@gmail.com>
2025-12-13 05:18:25 +0100

    Add BreadCrumb

--- frontend/src/pages/ShowsPage.tsx
+++ frontend/src/pages/ShowsPage.tsx
@@ -1,9 +1,11 @@
+import Breadcrumb from '../components/Breadcrumb.tsx';
 import ShowGrid from '../components/ShowGrid.tsx';
 
 export default function ShowsPage() {
   return (
     <>
-      <p className="text-3xl">TV Shows</p>
+      <Breadcrumb />
+      <p className="mt-2 text-3xl">TV Shows</p>
       <ShowGrid />
     </>
   );
