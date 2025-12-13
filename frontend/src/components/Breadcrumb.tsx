7ff7ed7477ff8e813c0cc87d7eda2c8954893f26
Stanislav Dering <stanislavdering@gmail.com>
2025-12-13 05:38:31 +0100

    Add ShowsPageTitel

--- frontend/src/components/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumb.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 export default function Breadcrumb() {
   return (
     <div>
-      <Link to="/shows" className="text-sky-600 hover:underline">
+      <Link to="/shows" className="text-sm text-sky-600 hover:underline">
         Home
       </Link>
       <span className="mx-2 text-neutral-400">&gt;</span>
--- frontend/src/pages/ShowsPage.tsx
+++ frontend/src/pages/ShowsPage.tsx
@@ -1,11 +1,12 @@
 import Breadcrumb from '../components/Breadcrumb.tsx';
 import ShowGrid from '../components/ShowGrid.tsx';
+import ShowsPageTitel from '../components/ShowsPageTitel.tsx';
 
 export default function ShowsPage() {
   return (
     <>
       <Breadcrumb />
-      <p className="mt-2 text-3xl">TV Shows</p>
+      <ShowsPageTitel />
       <ShowGrid />
     </>
   );
