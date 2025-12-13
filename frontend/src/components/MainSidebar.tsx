1c61eb0cf8c5ae6f173cf94779df62caba1c3e00
Stanislav Dering <stanislavdering@gmail.com>
2025-12-13 19:08:12 +0100

    Add MainSidebar items

--- frontend/src/components/MainSidebar.tsx
+++ frontend/src/components/MainSidebar.tsx
@@ -1,8 +1,21 @@
+import { Gi3dGlasses, GiBookCover } from 'react-icons/gi';
+import { IoGameController } from 'react-icons/io5';
+import { PiMonitorPlayFill } from 'react-icons/pi';
+import { RiMovie2Fill } from 'react-icons/ri';
+
+import MainSidebarItem from './MainSidebarItem.tsx';
+
 export default function MainSidebar() {
   return (
     <aside
       className="fixed top-(--sidebar-y-indent) left-(--sidebar-x-indent)
         h-(--sidebar-height) w-(--main-sidebar-width) bg-gray-150"
-    ></aside>
+    >
+      <MainSidebarItem Icon={PiMonitorPlayFill} label="My Shows" />
+      <MainSidebarItem Icon={RiMovie2Fill} label="My Movies" />
+      <MainSidebarItem Icon={IoGameController} label="My Games" />
+      <MainSidebarItem Icon={GiBookCover} label="My Books" />
+      <MainSidebarItem Icon={Gi3dGlasses} label="Profile" />
+    </aside>
   );
 }
