fa4e6738e98151a43faac2b006cd970462ec4193
Stanislav Dering <stanislavdering@gmail.com>
2025-12-14 16:18:40 +0100

    Update MainSidebarItem style

--- frontend/src/components/MainSidebarItem.tsx
+++ frontend/src/components/MainSidebarItem.tsx
@@ -8,12 +8,14 @@ interface Props {
 export default function MainSidebarItem({ Icon, label }: Readonly<Props>) {
   return (
     <div
-      className="flex flex-col items-center border-b border-neutral-400 pt-5
-        pb-2 text-neutral-400 transition hover:cursor-pointer hover:bg-white
+      className="group flex flex-col items-center border-b border-neutral-400
+        pt-5 pb-2 text-[#d4d4d4] transition hover:cursor-pointer hover:bg-white
         hover:text-red-700"
     >
       <Icon size={40} />
-      <div className="text-sm">{label}</div>
+      <div className="text-sm text-neutral-400 group-hover:text-black">
+        {label}
+      </div>
     </div>
   );
 }
