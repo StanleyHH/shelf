6163cd7ee1c913868e55997f243a3da950193b81
Stanislav Dering <stanislavdering@gmail.com>
2025-12-14 18:36:49 +0100

    Add ad to secondary sidebar and update Breadcrumb

--- frontend/src/components/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumb.tsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router';
 
-export default function Breadcrumb() {
+interface Props {
+  currentPage?: string;
+}
+
+export default function Breadcrumb({ currentPage }: Readonly<Props>) {
   return (
-    <div className="text-sm">
+    <div className="flex text-sm">
       <Link to="/shows" className="text-sky-600 hover:underline">
         Home
       </Link>
@@ -10,6 +14,12 @@ export default function Breadcrumb() {
       <Link to="/shows" className="text-sky-600 hover:underline">
         Shows
       </Link>
+      {currentPage === 'showDetails' && (
+        <div>
+          <span className="mx-2 text-neutral-400">&gt;</span>
+          <span className="text-neutral-400">Solar Opposites</span>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/pages/ShowDetailsPage.tsx
+++ frontend/src/pages/ShowDetailsPage.tsx
@@ -1,10 +1,15 @@
+import yourAd from '../assets/your_ad.jpg';
+import Breadcrumb from '../components/Breadcrumb.tsx';
 import SecondSidebarContainer from '../components/SecondSidebarContainer.tsx';
 
 export default function ShowDetailsPage() {
   return (
     <>
+      <Breadcrumb currentPage="showDetails" />
       <div>ShowDetailsPage</div>
-      <SecondSidebarContainer></SecondSidebarContainer>
+      <SecondSidebarContainer>
+        <img src={yourAd} alt="your_ad" />
+      </SecondSidebarContainer>
     </>
   );
 }
