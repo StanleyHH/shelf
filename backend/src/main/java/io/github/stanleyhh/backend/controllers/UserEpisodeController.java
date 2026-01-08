134722cd4d38267f0136bd281c68cf6827c5b4bc
Stanislav Dering <stanislavdering@gmail.com>
2026-01-08 06:07:42 +0100

    Add addMyEpisode endpoint

---
+++ backend/src/main/java/io/github/stanleyhh/backend/controllers/UserEpisodeController.java
@@ -0,0 +1,28 @@
+package io.github.stanleyhh.backend.controllers;
+
+import io.github.stanleyhh.backend.services.UserEpisodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/me/episodes")
+public class UserEpisodeController {
+    private final UserEpisodeService userEpisodeService;
+
+    @PostMapping
+    public ResponseEntity<Void> addMyEpisodes(
+            @RequestBody List<Long> episodeIds,
+            @AuthenticationPrincipal OAuth2User oAuth2User) {
+        userEpisodeService.addMyEpisode(episodeIds, oAuth2User);
+        return ResponseEntity.noContent().build();
+    }
+}
--- backend/src/main/java/io/github/stanleyhh/backend/repositories/UserEpisodeRepository.java
+++ backend/src/main/java/io/github/stanleyhh/backend/repositories/UserEpisodeRepository.java
@@ -1,14 +1,20 @@
 package io.github.stanleyhh.backend.repositories;
 
 import io.github.stanleyhh.backend.domain.entities.Episode;
+import io.github.stanleyhh.backend.domain.entities.User;
 import io.github.stanleyhh.backend.domain.entities.UserEpisode;
 import io.github.stanleyhh.backend.domain.entities.embeddable.UserEpisodeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface UserEpisodeRepository extends JpaRepository<UserEpisode, UserEpisodeId> {
     boolean existsBy();
 
     List<UserEpisode> findAllByEpisode(Episode episode);
+
+    List<UserEpisode> findAllByUserAndEpisodeIn(User user, Collection<Episode> episodes);
+
+    List<UserEpisode> findAllByUser(User user);
 }
